74191c8995d7d0b74225acaf12b87bcdb9f1eea7
mohamed frikha <frikhamohamed95@gmail.com>
2025-10-28 10:48:06 +0100

    create spring project and configure db props

---
+++ src/main/java/com/ingcode/stock_management_spring/StockManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ingcode.stock_management_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingcode/stock_management_spring/StockManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingcode.stock_management_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
